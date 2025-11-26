592d1d878f78e6d7f56cd2d62341f5542f27f424
OisinLoftus-STUDENT <G00470518@atu.ie>
2025-11-26 16:51:39 +0000

    Created CustomerTest Class

---
+++ src/main/java/ie/atu/CustomerTest.java
@@ -0,0 +1,19 @@
+package ie.atu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer part1Customer = new Customer(
+                "Alice Smith",
+                "1 High Street",
+                "0851234567",
+                true,
+                1001
+        );
+
+
+
+    }
+}
